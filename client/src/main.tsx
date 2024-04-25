70e78f102d8aea926e895cba82e9228ff17bd29e
prince-rgt <prince@reallygreattech.com>
2024-04-25 15:14:48 +0100

    Minor Changes

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,12 +2,16 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
 import AppRoutes from "./routes/index.tsx"
-import "./index.css"
+import {Provider} from "react-redux"
+import store from "./redux/store.ts"
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <Provider store={store} >
       <BrowserRouter>
         <AppRoutes />
       </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
