7b55c0393f95f0163e6336526ba21065fbf8c6c6
andrews-408 <yeboahandrews408@gmail.com>
2024-04-26 13:11:57 +0000

    added message collection

--- server/src/controllers/chat.controller.ts
+++ server/src/controllers/chat.controller.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from "express";
-import { createNewChat } from "../Service/chat.service";
+import { createNewChat, deleteChat, getUserChats } from "../Service/chat.service";
 
 export const initiateChat = async (req: Request, res: Response, next: NextFunction) => {
     try {
@@ -12,3 +12,27 @@ export const initiateChat = async (req: Request, res: Response, next: NextFuncti
         return next(error);
     }
   };
+
+export const getChatHistory = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const results = await getUserChats(req);
+        if(results.status !== "success"){
+            return res.status(400).json(results);
+        }
+        return res.status(200).json(results)
+    } catch(error) {
+        return next(error);
+    }
+}
+
+export const clearChat = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const results = await deleteChat(req);
+        if(results.status !== "success") {
+            return res.status(400).json(results)
+        }
+        return res.status(200).json(results)
+    } catch(error){
+        return next(error)
+    }
+}
