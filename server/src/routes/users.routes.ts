f8681bbf440adb692347ae1abe4c5c64ec6ee5e7
Alex <alexadam269@gmail.com>
2024-04-26 10:54:28 +0000

    user profile update feature implemented

--- server/src/routes/users.routes.ts
+++ server/src/routes/users.routes.ts
@@ -1,12 +1,23 @@
-import express from 'express';
-import { createUser, login, forgotPassword, verifyToken, resetPassword } from '../controllers/users.controllers';
+import { updateUserProfile } from "../controllers/users.controllers";
+
+import express from "express";
+import {
+  createUser,
+  login,
+  forgotPassword,
+  verifyToken,
+  resetPassword,
+} from "../controllers/users.controllers";
+import auth from "../middlewares/auth";
 
 const router = express.Router();
 
-router.post('/register', createUser);
-router.post('/login', login);
-router.post('/forgot-password', forgotPassword);
-router.post('/reset-password/:token', resetPassword);
-router.get('/verify/:token', verifyToken);
+router.post("/register", createUser);
+router.put("/profile", auth, updateUserProfile);
+
+router.post("/login", login);
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password/:token", resetPassword);
+router.get("/verify/:token", verifyToken);
 
 export default router;
