02cd6a228a4e87a80bedef161f945c680e01bd04
prince-rgt <prince@reallygreattech.com>
2024-04-29 12:19:32 +0100

    Added Patients Report Page

---
+++ client/src/data/patientsReportMockData.ts
@@ -0,0 +1,47 @@
+// Prediction: 0 => No disease present
+// Prediction: 1 => Yes
+
+export const PatientsReportData = [
+    {   
+        id: 's8Ddfd9sdDE9dW',
+        name: 'Prince Mireku',
+        prediction: 1,
+        cLevel: 0.5, // Confidence Level
+        dateCreated: '2024-04-29',
+    },
+    {
+        id: 's8Ddfd9sdDE9dW',
+        name: 'Jane Doe',
+        prediction: 1,
+        cLevel: 0.8,
+        dateCreated: '2024-04-29',
+    },
+    {
+        id: 's8Ddfd9sdDE9dX',
+        name: 'John Smith',
+        prediction: 0,
+        cLevel: 0.6,
+        dateCreated: '2024-04-29',
+    },
+    {
+        id: 's8Ddfd9sdDE9dY',
+        name: 'Alice Johnson',
+        prediction: 1,
+        cLevel: 0.7,
+        dateCreated: '2024-04-29',
+    },
+    {
+        id: 's8Ddfd9sdDE9dZ',
+        name: 'Bob Brown',
+        prediction: 0,
+        cLevel: 0.4,
+        dateCreated: '2024-04-29',
+    },
+    {
+        id: 's8Ddfd9sdDE9da',
+        name: 'Emily Davis',
+        prediction: 1,
+        cLevel: 0.9,
+        dateCreated: '2024-04-29',
+    },
+]
--- client/src/pages/PatientReports.tsx
+++ client/src/pages/PatientReports.tsx
@@ -1,28 +1,74 @@
-const PatientReports = () => {
-  return (
-   <section className="bg-white dark:bg-gray-900 mt-5 min-h-[83vh] flex  justify-center  ">
-      <div className="py-8 px-4 mx-auto max-w-screen-md text-center lg:py-16 lg:px-12">
-        <svg
-          className="mx-auto mb-4 w-10 h-10 text-gray-400"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 512 512"
-        >
-          <path
-            fill="currentColor"
-            d="M331.8 224.1c28.29 0 54.88 10.99 74.86 30.97l19.59 19.59c40.01-17.74 71.25-53.3 81.62-96.65c5.725-23.92 5.34-47.08 .2148-68.4c-2.613-10.88-16.43-14.51-24.34-6.604l-68.9 68.9h-75.6V97.2l68.9-68.9c7.912-7.912 4.275-21.73-6.604-24.34c-21.32-5.125-44.48-5.51-68.4 .2148c-55.3 13.23-98.39 60.22-107.2 116.4C224.5 128.9 224.2 137 224.3 145l82.78 82.86C315.2 225.1 323.5 224.1 331.8 224.1zM384 278.6c-23.16-23.16-57.57-27.57-85.39-13.9L191.1 158L191.1 95.99l-127.1-95.99L0 63.1l96 127.1l62.04 .0077l106.7 106.6c-13.67 27.82-9.251 62.23 13.91 85.39l117 117.1c14.62 14.5 38.21 14.5 52.71-.0016l52.75-52.75c14.5-14.5 14.5-38.08-.0016-52.71L384 278.6zM227.9 307L168.7 247.9l-148.9 148.9c-26.37 26.37-26.37 69.08 0 95.45C32.96 505.4 50.21 512 67.5 512s34.54-6.592 47.72-19.78l119.1-119.1C225.5 352.3 222.6 329.4 227.9 307zM64 472c-13.25 0-24-10.75-24-24c0-13.26 10.75-24 24-24S88 434.7 88 448C88 461.3 77.25 472 64 472z"
-          />
-        </svg>
-        <h1 className="mb-4 text-4xl font-bold tracking-tight leading-none text-gray-900 lg:mb-6 md:text-5xl xl:text-6xl dark:text-white">
-          Patient Reports Page <br />
-          Under Construction
-        </h1>
-        <p className="font-light text-gray-500 md:text-lg xl:text-xl dark:text-gray-400">
-          Our Developers are working earnestly to get the Patient Reports Page and it's
-          features up and Running.
-        </p>
-      </div>
-    </section>
-  )
-}
+import { PatientsReportData } from "../data/patientsReportMockData";
+import { RiEdit2Line } from "react-icons/ri";
+import { MdDelete } from "react-icons/md";
+import { BsStars } from "react-icons/bs";
 
-export default PatientReports
+const formatPredictionConfidenceLevel = (level: number): string => {
+    return (level * 100).toFixed(0) + '%';
+};
+
+const PatientReports: React.FC = () => {
+    return (
+        <section className="bg-white dark:bg-gray-900 mt-5 min-h-[83vh] flex flex-col">
+            <div className="mx-2 my-4">
+                <nav className="flex" aria-label="Breadcrumb">
+                    <ol className="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
+                        <li className="inline-flex items-center">
+                            <a href="#" className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
+                                Doctor
+                            </a>
+                        </li>
+                        <li>
+                            <div className="flex items-center">
+                                <svg className="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+                                </svg>
+                                <a href="#" className="ms-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ms-2 dark:text-gray-400 dark:hover:text-white">Reports</a>
+                            </div>
+                        </li>
+                    </ol>
+                </nav>
+            </div>
+
+            <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+                <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 table-fixed">
+                    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-200">
+                        <tr>
+                            <th className="px-6 py-3 w-1/6">Name</th>
+                            <th className="px-6 py-3 w-1/6">Prediction</th>
+                            <th className="px-6 py-3 w-1/6">Confidence Level</th>
+                            <th className="px-6 py-3 w-1/6">Report Created At</th>
+                            <th className="px-6 py-3 w-1/6">AI/ PalmGPT</th>
+                            <th className="px-6 py-3 w-1/6">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        {PatientsReportData.map((patient, index) => (
+                            <tr key={patient.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                <td className="px-6 py-4 w-1/6">{patient.name}</td>
+                                <td className="px-6 py-4 w-1/6">{
+									patient.prediction === 1 ? 'Yes, diesease likely present' : 'No disease present'
+								}</td>
+                                <td className="px-6 py-4 w-1/6">{formatPredictionConfidenceLevel(patient.cLevel)}</td>
+                                <td className="px-6 py-4 w-1/6">{patient.dateCreated}</td>
+								 <td className="px-6 py-4 w-1/6">
+									<button className="p-2 rounded-lg hover:bg-gray-500/20 dark:hover:bg-gray-500/50"> 
+										<BsStars size={18} /> 
+									</button>
+								 </td>
+								 <td className="pl-3 py-4 w-1/6 flex space-x-4 items-center">
+                                        <button className="hover:text-green-400" > <RiEdit2Line size="1.4em"/> </button>
+                                        <button className="hover:text-red-600"> <MdDelete size="1.4em"/> </button>
+                                 </td>
+                            </tr>
+                        ))}
+
+                    </tbody>
+                </table>
+            </div>
+        </section>
+    );
+};
+
+export default PatientReports;
