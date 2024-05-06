6e1bd7616ebaaafe3026aeb97f092d2f51911a63
CLAUDIOUS OPOKU MENSAH <claudiouso00@gmail.com>
2024-05-06 07:56:41 +0000

    slash screen

---
+++ mobile/palmed/lib/Auth/presentation/views/screen_one.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+
+class FirstView extends StatelessWidget {
+  final AnimationController animationController;
+
+  const FirstView({super.key, required this.animationController});
+
+  @override
+  Widget build(BuildContext context) {
+    final firstHalfAnimation =
+        Tween<Offset>(begin: const Offset(1, 0), end: const Offset(0, 0))
+            .animate(CurvedAnimation(
+      parent: animationController,
+      curve: const Interval(
+        0.2,
+        0.4,
+        curve: Curves.fastOutSlowIn,
+      ),
+    ));
+    final secondHalfAnimation =
+        Tween<Offset>(begin: const Offset(0, 0), end: const Offset(-1, 0))
+            .animate(CurvedAnimation(
+      parent: animationController,
+      curve: const Interval(
+        0.4,
+        0.6,
+        curve: Curves.fastOutSlowIn,
+      ),
+    ));
+    final relaxFirstHalfAnimation =
+        Tween<Offset>(begin: const Offset(2, 0), end: const Offset(0, 0))
+            .animate(CurvedAnimation(
+      parent: animationController,
+      curve: const Interval(
+        0.2,
+        0.4,
+        curve: Curves.fastOutSlowIn,
+      ),
+    ));
+    final relaxSecondHalfAnimation =
+        Tween<Offset>(begin: const Offset(0, 0), end: const Offset(-2, 0))
+            .animate(CurvedAnimation(
+      parent: animationController,
+      curve: const Interval(
+        0.4,
+        0.6,
+        curve: Curves.fastOutSlowIn,
+      ),
+    ));
+
+    final imageFirstHalfAnimation =
+        Tween<Offset>(begin: const Offset(4, 0), end: const Offset(0, 0))
+            .animate(CurvedAnimation(
+      parent: animationController,
+      curve: const Interval(
+        0.2,
+        0.4,
+        curve: Curves.fastOutSlowIn,
+      ),
+    ));
+    final imageSecondHalfAnimation =
+        Tween<Offset>(begin: const Offset(0, 0), end: const Offset(-4, 0))
+            .animate(CurvedAnimation(
+      parent: animationController,
+      curve: const Interval(
+        0.4,
+        0.6,
+        curve: Curves.fastOutSlowIn,
+      ),
+    ));
+
+    return SlideTransition(
+      position: firstHalfAnimation,
+      child: SlideTransition(
+        position: secondHalfAnimation,
+        child: Padding(
+          padding: const EdgeInsets.only(bottom: 100),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SlideTransition(
+                position: imageFirstHalfAnimation,
+                child: SlideTransition(
+                  position: imageSecondHalfAnimation,
+                  child: Container(
+                    constraints:
+                        const BoxConstraints(maxWidth: 350, maxHeight: 250),
+                    child: Image.asset(
+                      'assets/introduction_animation/one.png',
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                ),
+              ),
+              SlideTransition(
+                position: relaxFirstHalfAnimation,
+                child: SlideTransition(
+                  position: relaxSecondHalfAnimation,
+                  child: const Text(
+                    "Final Verdict and Report Generation",
+                    style:
+                        TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              const Padding(
+                padding:
+                    EdgeInsets.only(left: 64, right: 64, bottom: 16, top: 16),
+                child: Text(
+                  "Add personalized final verdicts to prediction model outcomes based on clinical expertise and patient-specific considerations.Generate comprehensive diagnostic reports incorporating prediction model results, doctor's final verdicts, and additional clinical notes.",
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
